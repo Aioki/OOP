53f7a1863f321605082b129f1423e3bdb4fc8c0f
Aioki <aioki@outlook.com>
2019-03-13 15:50:19 +0400

    Complete

--- Lab_03_Gulyaev/Lab_03_Gulyaev.cpp
+++ Lab_03_Gulyaev/Lab_03_Gulyaev.cpp
@@ -7,9 +7,49 @@
 
 int main()
 {
-	int a[20];
-	int x;
+	int a[20]; // 
+	int M; //Максимальный размер массива
+	int max1, max2=1001; //Максимальное и предмаксимальное значение 
+	
 
+	input_printf("input the size of the array ");
+	scanf("%d", &M); // ввод размера массива
+	if (M < 2 || M>20) //проверка размера массива
+	{
+		error_printf("wrong input numbers");
+		return 0;
+	}
+
+	printf("input elements of the array A ");
+	for (int i = 0; i < M; i++) //цикл ввода массива
+	{
+		scanf("%d", &a[i]); //чтение элементов массива
+		if (a[i] > 1000 || a[i] < -1000) //проверка элементов массива
+		{
+			error_printf("wrong elements of the array");
+			return 0;
+		}
+	}
+
+	max1 = a[0];
+
+
+	for (int i = 1; i < M; i++) { //цикл поиска 
+		if (a[i] > max1) {
+
+			max2 = max1;
+			max1 = a[i];
+
+		}
+	}
+
+	if (max2 = 1001) {
+		error_printf("No solution");
+		return 0;
+	}
+	else {
+		printf("%d", max2);
+	}
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
